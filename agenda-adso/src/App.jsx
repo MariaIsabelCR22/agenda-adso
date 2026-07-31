f1479a9e99afbcff9ee8d9ab28a772fc4a3b9d16
Maria Isabel <holaah565@gmail.com>
2026-07-30 23:40:08 -0500

    Clase_4_Agenda_ADSO_v2

---
+++ agenda-adso/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+import FormularioContacto from "./components/FormularioContacto";
+
+export default function App() {
+  const [contactos, setContactos] = useState([
+    {
+      id: 1,
+      nombre: "juanito alimaña",
+      telefono: "324 394 3856",
+      correo: "williecolon@gmail.com",
+      etiqueta: "Compañero",
+    },
+  ]);
+
+  // Agregar
+  const agregarContacto = (nuevo) => {
+    setContactos((prev) => [...prev, { id: Date.now(), ...nuevo }]);
+  };
+
+  // Eliminar
+  const eliminarContacto = (id) => {
+    setContactos((prev) => prev.filter((c) => c.id !== id));
+  };
+
+  return (
+    <main className="app-container">
+      <h1 className="app-title">Agenda ADSO v2</h1>
+
+      <FormularioContacto onAgregar={agregarContacto} />
+
+      <section className="lista-contactos">
+        {contactos.map((c) => (
+          <ContactoCard
+            key={c.id}
+            id={c.id}
+            nombre={c.nombre}
+            telefono={c.telefono}
+            correo={c.correo}
+            etiqueta={c.etiqueta}
+            onDelete={eliminarContacto}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
